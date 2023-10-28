db9704420e41cb2981e9a4eb5486a14364b3e14b
Mefrit <48288668+Mefrit@users.noreply.github.com>
2023-10-28 23:06:20 +0300

    Update main_class_models.py

--- app/models/main_class_models.py
+++ app/models/main_class_models.py
@@ -1,13 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-
-class WordList(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
 class NewWord(BaseModel):
     rus_value: str
     chinese_value: str 
